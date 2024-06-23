150028e9eb470d4eff80eb0819465b3a8cd2bb6c
Herson Lozano <hersonx@gmail.com>
2024-06-22 19:34:58 -0600

    delete Powershel

--- PowerShell/Scripts/p.ps1
+++
@@ -1,5 +0,0 @@
-write-host "bienvenidos"
-sleep(2)
-[Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition(0)
-[Microsoft.PowerShell.PSConsoleReadLine]::Insert("[Est el el inicio]")
-sleep(2)
--- PowerShell/Scripts/set-theme.ps1
+++
@@ -1,6 +0,0 @@
-
-param(
-    [string]$theme_name
-)
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\\${theme_name}.omp.json" | Invoke-Expression
