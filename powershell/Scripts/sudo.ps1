6f0a1d25268e67f953d1062fc2eb7821bf8bc1b6
Herson <hersonx@gmail.com>
2024-06-24 23:28:03 -0600

    UPDATE powershell/Scripts/sudo.ps1: se agrega la opcion, al final, de elegir quedarse en la terminal con permisos de administrador al presionar "ENTER" o salir al presionar cualquier tecla distinta de "ENTER"

--- powershell/Scripts/sudo.ps1
+++ powershell/Scripts/sudo.ps1
@@ -6,17 +6,30 @@ param(
 [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true, ValueFromRemainingArguments)]
 [string[]]$listaParametros)
 
-[string]$comando = ""
+[string]$comandos = ""
 
 if ($listaParametros -gt 0){
     ForEach ($parametro in $listaParametros){
         if ($parametro -match ".*\s.*"){
-            $comando = "${comando} '${parametro}'"
+            $comandos = "${comandos} '${parametro}'"
         }else{
-            $comando = "${comando} ${parametro}"
+            $comandos = "${comandos} ${parametro}"
         }
     }
-    start -verb runas -Path pwsh -ArgumentList '-noprofile','-noexit','-nologo',"-command ${comando}"
+    # ----------------------------------------------------------------------------
+    # lista de comandos a ejecutar el la terminal con permisos de administrador:
+    # ----------------------------------------------------------------------------
+    $lista_de_comandos = "
+        ${comandos};
+        Write-Host ' ';
+        Write-Host 'Presiona Enter para continuar ' -NoNewline -Foreground Blue;
+        Write-Host 'o ' -NoNewline;
+        Write-Host 'cualquier tecla para salir...' -Foreground Red;
+        `$key = `$Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+        if(`$key.VirtualKeyCode -ne 13) {[System.Environment]::Exit(0)};
+    "
+    # ----------------------------------------------------------------------------
+    start -verb runas -Path pwsh -ArgumentList '-noprofile','-noexit','-nologo',"-command ${lista_de_comandos}"
 }else{
     start -verb runas -Path pwsh -ArgumentList '-noprofile','-noexit','-nologo'
 }
