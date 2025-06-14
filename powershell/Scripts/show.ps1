6169292c1a3dee86b76b1695f65b8651cffb235e
Herson Lozano <hersonx@gmail.com>
2025-06-13 23:31:37 -0600

    se ha corregido un problema de "show.ps1", al agregarle la direccion base o Path del Get-ChildItem

--- powershell/Scripts/show.ps1
+++ powershell/Scripts/show.ps1
@@ -1,5 +1,4 @@
-
-$items = (Get-ChildItem *.ps1)
+$items = (Get-ChildItem -Path $PSScriptRoot "*.ps1" )
 $index = 0..($items.length - 1)
 $index | ForEach-Object{
         "{0,3} --- {1}" -f ($_ + 1),($items[$_].BaseName)
