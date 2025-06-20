71f15471290e0e0edde34e4b8d7c5f522bfea2bc
Herson Lozano <hersonx@gmail.com>
2025-06-20 14:04:37 -0600

    Se ha creado el script "a.ps1", para hacer pruebas sobre como automatizar la creación de Enlaces Simbólicos, también la gestión de que hacer cuando el enlace simbolico ya existe, etc

---
+++ powershell/Scripts/a.ps1
@@ -0,0 +1,38 @@
+﻿
+Param(
+    [string]$link
+)
+
+$shell = New-Object -ComObject WScript.Shell
+$miDocs = $shell.SpecialFolders("MyDocuments")
+$dirName = "aabb"
+$targen = "C:\aa"
+$link = Join-Path -Path $miDocs -ChildPath $dirName
+$dirOld = (Get-Location)
+
+Set-Location $miDocs
+
+if (Test-Path -Path $link) {
+    $item = Get-Item -Path $link -Force
+    if($item.LinkType -eq 'SymbolicLink'){
+        Write-Host "✔ La ruta [$link] es un enlace simbólico y apunta a:`n $($item.Target)"
+        Remove-Item $link -Force -Recurse
+        sudo New-Item -ItemType SymbolicLink -Path $link -Target $targen
+    }
+    else {
+        Write-Warning "⚠ La ruta [$link] existe, pero NO es un symlink (LinkType=$($item.LinkType))."
+        if (Test-Path -Path $link) {
+            Write-Warning "  ⚠  Ya existe un respaldo de `"$dirName`".  "
+            Remove-Item $link  -Force -Recurse
+        } else {
+            Rename-Item -Path $dirName -NewName "$dirName.original"
+        }
+        sudo New-Item -ItemType SymbolicLink -Path $link -Target $targen
+    }
+}
+else {
+    Write-Host "✘ No existe ningún archivo o carpeta llamado '$link'"
+    sudo New-Item -ItemType SymbolicLink -Path $link -Target $targen
+}
+
+Set-Location $dirOld
