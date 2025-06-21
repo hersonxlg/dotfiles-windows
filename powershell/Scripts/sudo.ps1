4a46ad5dd76e0c4644283f1e6c2673aa088ba257
Herson Lozano <hersonx@gmail.com>
2025-06-21 17:52:37 -0600

    Actualización del script "sudo.ps1", ahora el script selecciona la shell que se abrirá como administrador, usando "powershell.exe" si no existe "pwsh.exe", también ahora se pueden abrir "ScriptBlocks" o "Bloques de script" (que tendrían la siguiente sintaxis {...lista_de_instrucciones...})

--- powershell/Scripts/sudo.ps1
+++ powershell/Scripts/sudo.ps1
@@ -1,39 +1,103 @@
-
+﻿
 # *******************************************************
 #           comando "sudo"
 # *******************************************************
-param(
-[Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true, ValueFromRemainingArguments)]
-[string[]]$listaParametros)
 
-[string]$comandos = ""
+Param(
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]$parametros
+)
 
-if ($listaParametros -gt 0){
-    ForEach ($parametro in $listaParametros){
-        if ($parametro -match ".*\s.*"){
-            $comandos = "${comandos} '${parametro}'"
-        }else{
-            $comandos = "${comandos} ${parametro}"
-        }
+function showList([string[]]$lista){
+    $n = $lista.length
+    $i = @()
+    if($n -ne 0){
+        $i = @(0..($n-1))
     }
-    # ----------------------------------------------------------------------------
-    # lista de comandos a ejecutar el la terminal con permisos de administrador:
-    # ----------------------------------------------------------------------------
-    $lista_de_comandos = "
-        ${comandos};
-        Write-Host ' ';
-        Write-Host 'Presiona Enter para continuar ' -NoNewline -Foreground Blue;
-        Write-Host 'o ' -NoNewline;
-        Write-Host 'cualquier otra tecla para salir...' -Foreground Red;
-        `$key = `$Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-        if(`$key.VirtualKeyCode -ne 13) {[System.Environment]::Exit(0)};
-    "
-    # ----------------------------------------------------------------------------
-    Start-Process -verb runas -FilePath pwsh -ArgumentList '-noprofile','-noexit','-nologo',"-command ${lista_de_comandos}"
-}else{
-    Start-Process -verb runas -FilePath pwsh -ArgumentList '-noprofile','-noexit','-nologo'
+    $i | ForEach-Object{ Write-Host ('{0,2}:{1}' -f ($_+1),$lista[$_]) -BackgroundColor Green -ForegroundColor Black }
 }
 
 
-# start -verb runas -Path pwsh -ArgumentList '-noprofile','-nologo',"-command ${comando} && sleep 2"
+# Eleva a administrador si aún no lo es
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+
+    $shell = "powershell.exe"
+    if(Get-Command pwshh.exe -ErrorAction SilentlyContinue) {
+        $shell = "pwsh.exe"
+    }
+
+    #-------------------------------------------------------
+    #  POWERSHELL.EXE
+    #-------------------------------------------------------
+    if($shell -eq "powershell.exe"){
+
+        $commandList  = @($parametros | ForEach-Object{'{0}' -f ($_ -replace '"','"""')})
+        if($parametros.length -gt 1){
+            $commandList = @($commandList | ForEach-Object{'{0}' -f ($_ -replace '""','""""')})
+            $commandList = @($commandList | ForEach-Object{ if($_ -match ' '){'""""{0}""""' -f $_}else{'"{0}"' -f $_} })
+        }else{
+            #$commandList = @($commandList | ForEach-Object{'{0}' -f ($_ -replace '"','""')})
+            $commandList = @($commandList | ForEach-Object{'"" {0} ""' -f $_})
+        }
+
+    }
+
+    #-------------------------------------------------------
+    #  PWSH.EXE
+    #-------------------------------------------------------
+    else{
+        "pwsh.exe"
+        if($parametros.length -gt 1){
+            $commandList  = @($parametros | ForEach-Object{'{0}' -f ($_ -replace '"','""""')})
+            ##$commandList = @($commandList | ForEach-Object{'{0}' -f ($_ -replace '""','""""')})
+            $commandList = @($commandList | ForEach-Object{ if($_ -match ' '){'"""{0}"""' -f $_}else{'"{0}"' -f $_} })
+        }else{
+            $commandList = @($parametros | ForEach-Object{'{0}' -f ($_ -replace '"','""')})
+            $commandList = @($commandList | ForEach-Object{'"{0}"' -f $_})
+        }
+    }
+
+
+    $shellArgList  = @()
+    $shellArgList += "-WindowStyle"
+    $shellArgList += "Maximized"
+    $shellArgList += "-ExecutionPolicy"
+    $shellArgList += "Bypass"
+    $shellArgList += "-NoExit"
+    $shellArgList += "-NoLogo"
+    $shellArgList += "-NoProfile"
+    $shellArgList += "-File"
+    #$shellArgList += $PSScriptRoot
+    $shellArgList += $MyInvocation.MyCommand.Path
+    $shellArgList += $commandList
+
+    Start-Process -FilePath $shell -Wait -Verb RunAs -ArgumentList $shellArgList
+    
+    exit 0
+}
+
+
+$p = $parametros
+$a = $p -join ' '
+try{
+    iex $a
+
+    Write-Host
+    Write-Host 'Presiona Enter para continuar ' -NoNewline -Foreground Blue 
+    Write-Host 'o ' -NoNewline
+    Write-Host 'cualquier otra tecla para salir...' -Foreground Red
+    Write-Host 
+    $keyInfo = [System.Console]::ReadKey($true)
+    if ($keyInfo.Key -ne 'Enter') {
+        if($shell -eq "powershell.exe"){
+            exit 0
+        }else{
+            [System.Environment]::Exit(0)
+        }
+    }
+}catch{
+    Write-Host "`n  Ocurrio un ERROR  `n" -BackgroundColor Red -ForegroundColor Black
+    Sleep 4
+    [System.Environment]::Exit(1)
+}
 
