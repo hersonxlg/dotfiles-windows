be22399911bb3ec685b3c0eab1e0972bb564fea1
Herson Lozano <hersonx@gmail.com>
2025-06-21 17:53:29 -0600

    actualización del script "a.ps1"

--- powershell/Scripts/a.ps1
+++ powershell/Scripts/a.ps1
@@ -3,14 +3,33 @@ Param(
     [string]$link
 )
 
+
 $shell = New-Object -ComObject WScript.Shell
-$miDocs = $shell.SpecialFolders("MyDocuments")
+$dirBase = (Join-Path $shell.SpecialFolders("MyDocuments") "aa")
 $dirName = "aabb"
 $targen = "C:\aa"
-$link = Join-Path -Path $miDocs -ChildPath $dirName
+$link = Join-Path -Path $dirBase -ChildPath $dirName
 $dirOld = (Get-Location)
 
-Set-Location $miDocs
+# Corroborar que el directorio base existe:
+if (-not(Test-Path -Path $dirBase)) {
+    Write-Host "`n  La dirección [$dirBase], aún NO EXISTE  `n" -BackgroundColor Yellow -ForegroundColor Black 
+    New-Item -Type Directory $dirBase -Force | Out-Null
+    # Verificar si se creó exitosamente
+    if (Test-Path -Path $dirBase) {
+        Write-Host ""
+        Write-Host "✔ El directorio [$dirBase] fue creado con éxito." -BackgroundColor Green -ForegroundColor Black 
+        Write-Host ""
+    } else {
+        Write-Host ""
+        Write-Warning "✘ El directorio NO se creó."
+        Write-Host ""
+        exit 0
+    }
+}
+
+
+Set-Location $dirBase
 
 if (Test-Path -Path $link) {
     $item = Get-Item -Path $link -Force
@@ -33,6 +52,19 @@ if (Test-Path -Path $link) {
 else {
     Write-Host "✘ No existe ningún archivo o carpeta llamado '$link'"
     sudo New-Item -ItemType SymbolicLink -Path $link -Target $targen
+}
+
+
+# Verificar si el enlace simbolico se ha creado exitosamente:
+if (Test-Path -Path $link) {
+    Write-Host ""
+    Write-Host "  ✔ El directorio [$link] fue creado con éxito.  " -BackgroundColor Green -ForegroundColor Black 
+    Write-Host ""
+} else {
+    Write-Host ""
+    Write-Warning "✘ El directorio [$link] NO se creó."
+    Write-Host ""
+    exit 0
 }
 
 Set-Location $dirOld
