ded1d05ddaa3b6158b7c2d21f8d5b9a202b35a15
Herson Lozano <hersonx@gmail.com>
2025-06-22 00:11:47 -0600

    Se actualiza el Script "install.ps1", se crea un nuevo script de instalación, es cual tiene mas opcciones, incluso un menu de instalación

--- install.ps1
+++ install.ps1
@@ -1,76 +1,234 @@
+﻿
+Param(
+    [string]$link
+)
 
-# -----------------------------------------------------------------------------
-# POWERSHELL 7:
-# -----------------------------------------------------------------------------
+function pause
+{
+    Write-Host "`n--- Presiona cualquier tecla para continuar ---`n"
+    [void][System.Console]::ReadKey($true)
+}
 
+function install
+{
+    Param(
+        [string]$dirBase,
+        [string]$dirName,
+        [string]$target
+    )
 
+    $link = Join-Path -Path $dirBase -ChildPath $dirName
+    $dirOld = (Get-Location)
 
-if ($Host.Version.Major -eq 7){
-    #start powershell -ArgumentList ({sleep 2 ; .\install.ps1 })
-    $comandos = "cd ${pwd}; sleep 2 ; .\install.ps1; [System.Environment]::Exit(0)"
-    start -verb runas -FilePath powershell -ArgumentList '-noprofile','-noexit','-nologo',"-command ${comandos} "
-    [System.Environment]::Exit(0)
-}else{
-
-    $exis_command = (Get-Command pwsh -ErrorAction Ignore)
-
-    if ($exis_command -eq $null){
-        Write-Warning "El programa `"Powershell7`" no esta instalado todavía."
-    }else{
-        Write-Output "*****************************************************"
-        Write-Output "*   El programa `"Powershell7`" esta instalado.     *"
-        Write-Output "*****************************************************"
-        Write-Output "`n`n"
-        sleep 2
-        # "pwsh.exe" (POWERSHELL 7) está instalado en este sistema.
-        $documents_dir = ($PROFILE -replace '\\[^\\]+\\[^\\]+$','')
-        $powershell_dir = "${documents_dir}\\powershell"
-        if(Test-Path $powershell_dir){ 
-            # El directorio $powershell_dir EXISTE.
-            if( (Get-Item $powershell_dir).LinkType -eq $null) { 
-                # El directorio $powershell_dir NO es un enlace simbolico.
-                Write-Output "Se creara el enlace simbolico."
-                sleep 2
-                if( -not (Test-Path "${powershell_dir}.backup") ){
-                    Rename-Item $powershell_dir "${powershell_dir}.backup"
-                }
-                #New-Item -ItemType SymbolicLink -Path "${documents_dir}" -name "powershell" -Target ".\\powershell"
-                $comandos = "cd $(pwd) ;New-Item -ItemType SymbolicLink -Path '${documents_dir}' -name 'powershell' -Target 'powershell'"
-                $comandos = "${comandos}; [System.Environment]::Exit(0)"
-                Write-Output "${comandos}"
-                Write-Output "`n`n"
-                Write-Output "*****************************************************"
-                Write-Output "* Se creará el enlace simbolico hacia 'PowerShell'. *"
-                Write-Output "*****************************************************"
-                Write-Output " ~\documents\PowerShell                              "
-                Write-Output "*****************************************************"
-                Write-Output "`n`n"
-                sleep 5
-                start -verb runas -FilePath powershell -ArgumentList '-noprofile','-noexit','-nologo',"-command ${comandos}"
-            }else{
-                # El directorio $powershell_dir es un enlace simbolico.
-                Write-Output "`n`n"
-                Write-Output "*****************************************************"
-                Write-Output "* Se hace un pull del repositorio 'dotfiles-window' *"
-                Write-Output "*****************************************************"
-                Write-Output "`n`n"
-                sleep 2
-                git pull
-            }
-        }else{
-            # El directorio $powershell_dir NO EXISTE.
-                $comandos = "cd $(pwd) "
-                $comandos = "${comandos}; New-Item -ItemType SymbolicLink -Path '${documents_dir}' -name 'powershell' -Target 'powershell'"
-                $comandos = "${comandos}; [System.Environment]::Exit(0)"
-            start -verb runas -FilePath powershell -ArgumentList '-noprofile','-noexit','-nologo',"-command ${comandos}"
+    # Corroborar que el directorio base existe:
+    if (-not(Test-Path -Path $dirBase)) {
+        Write-Host "`n  La dirección [$dirBase], aún NO EXISTE  `n" -BackgroundColor Yellow -ForegroundColor Black 
+        New-Item -Type Directory $dirBase -Force | Out-Null
+        # Verificar si se creó exitosamente
+        if (Test-Path -Path $dirBase) {
+            Write-Host ""
+            Write-Host "✔ El directorio [$dirBase] fue creado con éxito." -BackgroundColor Green -ForegroundColor Black 
+            Write-Host ""
+        } else {
+            Write-Host ""
+            Write-Warning "✘ El directorio NO se creó."
+            Write-Host ""
+            exit 0
         }
+    }
+
+
+    Set-Location $dirBase
+
+    if (Test-Path -Path $link) {
+        $item = Get-Item -Path $link -Force
+        if($item.LinkType -eq 'SymbolicLink'){
+            Write-Host "✔ La ruta [$link] es un enlace simbólico y apunta a:`n $($item.Target)"
+            $opt = Read-Host "`n`nDesea cambiar el enlace simbolico? (Presione `"s`" para continar)"
+            if ($opt -eq 's') {
+                Write-Host "`n  Cambiando el enlace simbolico...  `n" -BackgroundColor Green -ForegroundColor Black
+                Remove-Item $link -Force -Recurse
+                New-Item -ItemType SymbolicLink -Path $link -Target $target
+            } else {
+                Write-Host "`n  El enlace simbolico NO se creará  `n" -BackgroundColor Green -ForegroundColor Black
+                return
+            }
+        }
+        else {
+            Write-Warning "⚠ La ruta [$link] existe, pero NO es un symlink (LinkType=$($item.LinkType))."
+            # Crear un respaldo
+            if (Test-Path -Path "$link.original") {
+                Write-Warning "  ⚠  Ya existe un respaldo de `"$dirName`".  "
+                Remove-Item $link  -Force -Recurse
+            } else {
+                Write-Warning "  ⚠  Creandose el respaldo de `"$dirName`"...  "
+                Rename-Item -Path "$link" -NewName "$link.original" -Force
+                #Copy-Item -Path "$link" -Destination "$link.original" -Recurse
+                #Remove-Item $link  -Force -Recurse
+                pause
+                ls vifm.original
+                pause
+                #Start-Sleep -Seconds 1
+            }
+            New-Item -ItemType SymbolicLink -Path $link -Target $target
+        }
+    }
+    else {
+        Write-Host "✘ No existe ningún archivo o carpeta llamado '$link'"
+        New-Item -ItemType SymbolicLink -Path $link -Target $target
+    }
+
+
+    # Verificar si el enlace simbolico se ha creado exitosamente:
+    if (Test-Path -Path $link) {
+        Write-Host ""
+        Write-Host "  ✔ El directorio [$link] fue creado con éxito.  " -BackgroundColor Green -ForegroundColor Black 
+        Write-Host ""
+    } else {
+        Write-Host ""
+        Write-Warning "✘ El directorio [$link] NO se creó."
+        Write-Host ""
+        exit 0
+    }
+
+    Set-Location $dirOld
+}
+
+#---------------------------------------------------------------------------
+# INICIO
+#---------------------------------------------------------------------------
+
+
+# Eleva a administrador si aún no lo es
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+
+    $shell = "powershell.exe"
+    if(Get-Command pwsh.exe -ErrorAction SilentlyContinue) {
+        $shell = "pwsh.exe"
+    }
+
+    $shellArgList  = @()
+    #$shellArgList += "-NoExit"
+    $shellArgList += "-WindowStyle"
+    $shellArgList += "Maximized"
+    $shellArgList += "-ExecutionPolicy"
+    $shellArgList += "Bypass"
+    $shellArgList += "-NoLogo"
+    $shellArgList += "-NoProfile"
+    $shellArgList += "-File"
+    $shellArgList += $MyInvocation.MyCommand.Path
+    #$shellArgList += $commandList
+
+    Start-Process -FilePath $shell -Wait -Verb RunAs -ArgumentList $shellArgList
+    
+    exit 0
+
+}
+
+# VARIABLES:
+$shell = New-Object -ComObject WScript.Shell
+$dirNames = @(
+        "powershell",
+        "vifm",
+        "nvim"
+    )
+$dirBases = @(
+        $shell.SpecialFolders("MyDocuments"), # powershell
+        $env:APPDATA,                         # vifm
+        $env:LOCALAPPDATA                     # nvim
+    )
+$PSScriptRoot
+$targets = @(
+        (Join-Path $PSScriptRoot $dirNames[0]), # powershell
+        (Join-Path $PSScriptRoot $dirNames[1]), # vifm
+        (Join-Path $PSScriptRoot $dirNames[2])  # nvim
+    )
+$dirNames
+$dirBases
+$targets
+$targets | ForEach-Object{
+    if ( -not(Test-Path -Path $_ )) {
+        Write-Host "`n  El archivo [" -NoNewline -BackgroundColor Red -ForegroundColor Black
+        Write-Host "$_" -NoNewline -BackgroundColor Red -ForegroundColor White
+        Write-Host "] NO existe.  `n" -BackgroundColor Red -ForegroundColor Black
+        # Pausa antes de salir...
+        Write-Host "`n--- Presiona cualquier tecla para salir ---`n"
+        [void][System.Console]::ReadKey($true)
+        exit 1
     }
 }
 
 
-# Abrir de nuevo powershell 7:
-wt -d "$(pwd)"
+#Write-Host "`n--- Presiona cualquier tecla para salir ---`n"
+#[void][System.Console]::ReadKey($true)
+#exit
 
-# -----------------------------------------------------------------------------
-# neovim:
-# -----------------------------------------------------------------------------
+
+do {
+    Clear-Host
+    Write-Host "==== MENÚ PRINCIPAL ===="
+    Write-Host "1. Instalar powershell"
+    Write-Host "2. Instalar vifm"
+    Write-Host "3. Instalar neovim"
+    Write-Host "a. Instalar TODO"
+    Write-Host "0. Salir"
+    Write-Host ""
+    $opcion = Read-Host "Elige una opción:"
+
+    switch ($opcion) {
+        '1' {
+            Clear-Host
+            Write-Host
+            Write-Host "Instalando PowerShell`n"
+            # Iniciar la creación de los enlaces simbolicos:
+            install $dirBases[0] $dirNames[0] $targets[0]
+            # Pausa antes de salir...
+            Write-Host "`n--- Presiona cualquier tecla para regresar al menu ---`n"
+            [void][System.Console]::ReadKey($true)
+        }
+        '2' {
+            Clear-Host
+            Write-Host
+            Write-Host "Instalando Vifm`n"
+            # Iniciar la creación de los enlaces simbolicos:
+            install $dirBases[1] $dirNames[1] $targets[1]
+            Write-Host "`n--- Presiona cualquier tecla para regresar al menu ---`n"
+            [void][System.Console]::ReadKey($true)
+        }
+        '3' {
+            Clear-Host
+            Write-Host
+            Write-Host "Instalando Neovim`n"
+            # Iniciar la creación de los enlaces simbolicos:
+            install $dirBases[2] $dirNames[2] $targets[2]
+            Write-Host "`n--- Presiona cualquier tecla para regresar al menu ---`n"
+            [void][System.Console]::ReadKey($true)
+        }
+        'a' {
+            Clear-Host
+            Write-Host
+            Write-Host "Instalando TODO"
+            Write-Host "`n--- Presiona cualquier tecla para regresar al menu ---`n"
+            [void][System.Console]::ReadKey($true)
+        }
+        '0' {
+            Clear-Host
+            Write-Host
+            Write-Host "Saliendo del menú..."
+        }
+        default {
+            Clear-Host
+            Write-Host
+            Write-Host "❌ Opción no válida. Intenta de nuevo."
+            Write-Host "`n--- Presiona cualquier tecla para regresar al menu ---`n"
+            [void][System.Console]::ReadKey($true)
+        }
+    }
+} while ($opcion -ne '0')
+
+
+
+# Pausa antes de salir...
+Write-Host "`n--- Presiona cualquier tecla para salir ---`n"
+[void][System.Console]::ReadKey($true)
+
