f873be835ecd7e70cbdc197f920d73b78c8c5f45
Herson Lozano <hersonx@gmail.com>
2025-06-22 11:14:05 -0600

    update del script "edit-path.ps1"

--- powershell/Scripts/edit-path.ps1
+++ powershell/Scripts/edit-path.ps1
@@ -5,10 +5,23 @@ param(
     [string]$Scope
     )
 
+# VARIABLES:
+$path_file_name = "_var_paths.txt"
 
 if ( $Scope -eq "" ) {
     $Scope = "Machine"
 }
+
+$editor = "notepad.exe"
+if(Get-Command nvim -ErrorAction SilentlyContinue){
+	$editor = "nvim"
+}
+
+$editCommand = "$editor $path_file_name"
+if($editor -eq "notepad.exe"){
+    $editCommand = "Start-Process -Wait -WindowStyle Maximized $editor $path_file_name"
+}
+
 
 $path_list = ($env:path -split ';' )
 $dirs_machine = [System.Environment]::GetEnvironmentVariable("path", "Machine")
@@ -22,20 +35,20 @@ $the_rest = ($the_rest_list -join ';')
 $comando = ""
 
 if ( $Scope -eq "Machine" ) {
-    Write-Output $dirs_machine_list >"_Path_Machine.txt"
-    vim "_Path_Machine.txt"
-    $dirs_machine = ((Get-Content "_Path_Machine.txt") -join ';' )
-    Remove-Item "_Path_Machine.txt"
+    Write-Output $dirs_machine_list >"$path_file_name"
+    Invoke-Expression $editCommand
+    $dirs_machine = ((Get-Content "$path_file_name") -join ';' )
+    Remove-Item "$path_file_name"
     $comando = "[Environment]::SetEnvironmentVariable('path', '${dirs_machine}', 'Machine')"
 }else{
-    Write-Output $dirs_user_list >"_Path_User.txt"
-    vim "_Path_User.txt"
-    $dirs_user = ((Get-Content "_Path_User.txt") -join ';' )
-    Remove-Item "_Path_User.txt"
+    Write-Output $dirs_user_list >"$path_file_name"
+    Invoke-Expression $editCommand
+    $dirs_user = ((Get-Content "$path_file_name") -join ';' )
+    Remove-Item "$path_file_name"
     $comando = "[Environment]::SetEnvironmentVariable('path', '${dirs_user}', 'User')"
 }
 
-Start-Process -wait -verb runas -Path pwsh -ArgumentList '-noprofile','-nologo',"-command `"${comando}`" "
+Start-Process -wait -verb runas -Path pwsh -ArgumentList @('-noprofile','-nologo',"-command `"${comando}`" ")
 $env:path = "${the_rest};${dirs_machine};${dirs_user}"
 
 
