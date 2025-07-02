f75c9b4b72ab997930da1e96f658cc55a359d0d4
Herson Lozano <hersonx@gmail.com>
2025-07-01 21:50:42 -0600

    Se actuliza el script de configuración de PowerShell "Microsoft.PowerShell_profile.ps1", este script simplemente se formatea, no se le agrega o cambia NADA.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -107,92 +107,82 @@ $env:FZF_DEFAULT_OPTS=@"
 --bind ctrl-e:toggle-preview
 "@
 
-function _open_path
-{
-  param (
-    [string]$input_path
-  )
-  if (-not $input_path)
-  {
-    return
-  }
-  Write-Output "[ ] cd"
-  Write-Output "[*] nvim"
-  $choice = Read-Host "Enter your choice"
-  if ($input_path -match "^.*:\d+:.*$")
-  {
-    $input_path = ($input_path -split ":")[0]
-  }
-  switch ($choice)
-  {
-    {$_ -eq "" -or $_ -eq " "}
-    {
-      if (Test-Path -Path $input_path -PathType Leaf)
-      {
-        $input_path = Split-Path -Path $input_path -Parent
-      }
-      Set-Location -Path $input_path
+function _open_path {
+    param (
+        [string]$input_path
+    )
+    if (-not $input_path) {
+        return
     }
-    default
-    { nvim $input_path
+    Write-Output "[ ] cd"
+    Write-Output "[*] nvim"
+    $choice = Read-Host "Enter your choice"
+    if ($input_path -match "^.*:\d+:.*$") {
+        $input_path = ($input_path -split ":")[0]
     }
-  }
+    switch ($choice) {
+        {$_ -eq "" -or $_ -eq " "} {
+            if (Test-Path -Path $input_path -PathType Leaf) {
+                $input_path = Split-Path -Path $input_path -Parent
+            }
+            Set-Location -Path $input_path
+        }
+        default {
+            nvim $input_path
+        }
+    }
 }
 
-function _get_path_using_fd
-{
-  $input_path = fd --type file --follow --hidden --exclude .git |
-    fzf --prompt 'Files> ' `
-      --header-first `
-      --header 'CTRL-S: Switch between Files/Directories' `
-      --bind 'ctrl-s:transform:if not "%FZF_PROMPT%"=="Files> " (echo ^change-prompt^(Files^> ^)^+^reload^(fd --type file^)) else (echo ^change-prompt^(Directory^> ^)^+^reload^(fd --type directory^))' `
-      --preview 'if "%FZF_PROMPT%"=="Files> " (bat --color=always {} --style=plain) else (eza -T --colour=always --icons=always {})'
-  return $input_path
+function _get_path_using_fd {
+    $input_path = fd --type file --follow --hidden --exclude .git |
+        fzf --prompt 'Files> ' `
+            --header-first `
+            --header 'CTRL-S: Switch between Files/Directories' `
+            --bind 'ctrl-s:transform:if not "%FZF_PROMPT%"=="Files> " (echo ^change-prompt^(Files^> ^)^+^reload^(fd --type file^)) else (echo ^change-prompt^(Directory^> ^)^+^reload^(fd --type directory^))' `
+            --preview 'if "%FZF_PROMPT%"=="Files> " (bat --color=always {} --style=plain) else (eza -T --colour=always --icons=always {})'
+    return $input_path
 }
 
-function _get_path_using_rg
-{
-  $INITIAL_QUERY = "${*:-}"
-  $RG_PREFIX = "rg --column --line-number --no-heading --color=always --smart-case"
-  $input_path = "" |
-    fzf --ansi --disabled --query "$INITIAL_QUERY" `
-      --bind "start:reload:$RG_PREFIX {q}" `
-      --bind "change:reload:sleep 0.1 & $RG_PREFIX {q} || rem" `
-      --bind 'ctrl-s:transform:if not "%FZF_PROMPT%" == "1. ripgrep> " (echo ^rebind^(change^)^+^change-prompt^(1. ripgrep^> ^)^+^disable-search^+^transform-query:echo ^{q^} ^> %TEMP%\rg-fzf-f ^& cat %TEMP%\rg-fzf-r) else (echo ^unbind^(change^)^+^change-prompt^(2. fzf^> ^)^+^enable-search^+^transform-query:echo ^{q^} ^> %TEMP%\rg-fzf-r ^& cat %TEMP%\rg-fzf-f)' `
-      --color "hl:-1:underline,hl+:-1:underline:reverse" `
-      --delimiter ":" `
-      --prompt '1. ripgrep> ' `
-      --preview-label "Preview" `
-      --header 'CTRL-S: Switch between ripgrep/fzf' `
-      --header-first `
-      --preview 'bat --color=always {1} --highlight-line {2} --style=plain' `
-      --preview-window 'up,60%,border-bottom,+{2}+3/3'
-  return $input_path
+function _get_path_using_rg {
+    $INITIAL_QUERY = "${*:-}"
+    $RG_PREFIX = "rg --column --line-number --no-heading --color=always --smart-case"
+    $input_path = "" |
+        fzf --ansi --disabled --query "$INITIAL_QUERY" `
+            --bind "start:reload:$RG_PREFIX {q}" `
+            --bind "change:reload:sleep 0.1 & $RG_PREFIX {q} || rem" `
+            --bind 'ctrl-s:transform:if not "%FZF_PROMPT%" == "1. ripgrep> " (echo ^rebind^(change^)^+^change-prompt^(1. ripgrep^> ^)^+^disable-search^+^transform-query:echo ^{q^} ^> %TEMP%\rg-fzf-f ^& cat %TEMP%\rg-fzf-r) else (echo ^unbind^(change^)^+^change-prompt^(2. fzf^> ^)^+^enable-search^+^transform-query:echo ^{q^} ^> %TEMP%\rg-fzf-r ^& cat %TEMP%\rg-fzf-f)' `
+            --color "hl:-1:underline,hl+:-1:underline:reverse" `
+            --delimiter ":" `
+            --prompt '1. ripgrep> ' `
+            --preview-label "Preview" `
+            --header 'CTRL-S: Switch between ripgrep/fzf' `
+            --header-first `
+            --preview 'bat --color=always {1} --highlight-line {2} --style=plain' `
+            --preview-window 'up,60%,border-bottom,+{2}+3/3'
+    return $input_path
 }
 
-function fdg
-{
-  _open_path $(_get_path_using_fd)
+function fdg {
+    _open_path $(_get_path_using_fd)
 }
 
-function rgg
-{
-  _open_path $(_get_path_using_rg)
+function rgg {
+    _open_path $(_get_path_using_rg)
 }
 
 
 # SET KEYBOARD SHORTCUTS TO CALL FUNCTION
 
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -ScriptBlock {
-  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("fdg")
-  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("fdg")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 Set-PSReadLineKeyHandler -Key "Ctrl+g" -ScriptBlock {
-  [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-  [Microsoft.PowerShell.PSConsoleReadLine]::Insert("rgg")
-  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("rgg")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 
@@ -205,17 +195,17 @@ Set-PSReadLineKeyHandler -Key "Ctrl+g" -ScriptBlock {
 
 function whereis ($command) {
     Get-command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
 
 function getLatestVerison() {
-Param(
-    [Parameter(Mandatory=$true, Position=0)]
-    [string]
-    $module_name
-)
-$versions=@(cmd /c dir /O-N /b %USERPROFILE%\Documents\PowerShell\Modules\$module_name\);
-return $versions[0];
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]
+        $module_name
+    )
+    $versions=@(cmd /c dir /O-N /b %USERPROFILE%\Documents\PowerShell\Modules\$module_name\);
+    return $versions[0];
 }
 
