8e2170510307d0d52e10969eb89623f4bbfcb50f
Herson Lozano <hersonx@gmail.com>
2025-07-01 21:58:50 -0600

    se actuliza el código del script "lsdm.ps1", esto para obtener una pausa personalizada cuando los ficheros y directorios que se quieren mostrar no caben en la pantalla de la terminal

--- powershell/Scripts/lsdm.ps1
+++ powershell/Scripts/lsdm.ps1
@@ -2,8 +2,34 @@ if( -not(Get-Command lsd.exe -ErrorAction SilentlyContinue) ){
     Write-Error "  El programa `"lsd.exe`" se encuentra en el sistema...  "
 }
 
+$pagAlto = $Host.UI.RawUI.WindowSize.Height
+$pagAlto -= 3;
+
 try{
-    lsd.exe -lh --group-dirs=first | Out-Host -Paging
+    $linesWithoutPrint = @(lsd.exe -lh --group-dirs=first)
+    $numLinesWithoutPrint = $linesWithoutPrint.Length
+    do{
+        $indexLastLineToPrint = $pagAlto - 1
+        if($numLinesWithoutPrint -lt $pagAlto){ 
+            $indexLastLineToPrint = ($numLinesWithoutPrint - 1)
+        }
+        $linesToPrint = $linesWithoutPrint[0..($indexLastLineToPrint)]
+        if($numLinesWithoutPrint -eq ($indexLastLineToPrint+1)){
+            $linesWithoutPrint = @()
+        } else{
+            $linesWithoutPrint = $linesWithoutPrint[($indexLastLineToPrint+1)..($numLinesWithoutPrint-1)]
+        }
+        $numLinesWithoutPrint = $linesWithoutPrint.Length
+        Write-Host
+        $linesToPrint | ForEach-Object{Write-Host $_}
+        if($numLinesWithoutPrint -ne 0){
+            Write-Host
+            Write-Host ' -- Presiona Tecla "Espacio" para ir a la siguiente Pag. o "q" para salir --- '
+            $key = [System.Console]::ReadKey($true)
+            $char = $key.KeyChar;
+        }
+    }while(($char -ne 'q') -and ( $numLinesWithoutPrint -ne 0) )
+    
 } catch {
     Write-Host "`n  operación detenida: $_...   `n"
 }
