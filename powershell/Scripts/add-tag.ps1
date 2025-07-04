27eae57de5ed8cb66ddb81dabf52d0be3e52aad9
Herson Lozano <hersonx@gmail.com>
2025-07-04 11:57:00 -0600

    se ha modificado el script para que ahora utilice "ffmpeg.exe" para asignar las etiquetas a los ficheros (archivos) mp3

--- powershell/Scripts/add-tag.ps1
+++ powershell/Scripts/add-tag.ps1
@@ -3,15 +3,31 @@
 #             Comando "add-tag"
 # *******************************************************
 
-$artista = $($(ls -file).name | Select-String "^.+(?=\s-\s.+\.mp3)").Matches.Value | Select-String "."
-$titulo = $($(ls -file).name | Select-String "(?<=^.+\s-\s).+(?=\.mp3)").Matches.Value | Select-String "."
-$canciones = $(ls).FullName | Select-String ".+\s-\s.+\.mp3"
-$N = $canciones.length 
-for ( $i=0; $i -lt $canciones.Length; $i++)
-{
-	Write-Host "`n`n$($i+1)/$N`t$($titulo[$i]) : $($artista[$i])`n"
-	$argTagEditor = "kid3-cli `"$($canciones[$i])`" -c 'set title `"$($titulo[$i])`"' -c 'set artist `"$($artista[$i])`"' "
-	Invoke-Expression $argTagEditor
-	
+
+if( -not(Get-Command ffmpeg -ErrorAction SilentlyContinue)){
+    Write-Host "`n  No se ha dectectado el programa `"ffmpeg.exe`"...  `n" -BackgroundColor Red -ForegroundColor Black
+    exit 1;
 }
 
+$canciones = (Get-ChildItem -Filter "* - *.mp3")
+$N = $canciones.length 
+$i = 1;
+$canciones | ForEach-Object{
+
+    $audioFile = $_.FullName
+    $temp  = "$audioFile.tmp.mp3"
+
+    $artista,$titulo = ($_.BaseName) -split '\s+-\s+'
+    Write-Host "`n`n$($i)/$N`t$($titulo) : $($artista)`n"
+
+    ffmpeg -hide_banner -loglevel error -y -i $audioFile `
+        -metadata title="$titulo" `
+        -metadata artist="$artista" `
+        -c copy $temp
+    # -hide_banner    : oculta el encabezado de versión y configuración.
+    # -loglevel error : solo muestra mensajes en caso de fallo.
+    # -y              : Acepta automáticamente sobrescribir archivos de salida existentes sin preguntar.
+
+    Move-Item -Force $temp $audioFile
+    $i++;
+}
