a032d3a35ef72b236a635ab88bcfb8bc1d583c59
Herson-Laptop <hersonx@gmail.com>
2025-07-08 19:32:06 -0600

    se actualiza el script "profile.ps1", el cual solo contiene un script para cargar el entorno de "Anaconda" mediante el ejecutable "conda.exe", debido a que Anaconda no cargar la direccion de "conda.exe" en el PATH, es necesario indicar la direccion completa de "conda.exe" cuando se utilize en este script.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,8 +2,24 @@
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\ProgramData\anaconda3\Scripts\conda.exe") {
-    (& "C:\ProgramData\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+$ubicacionesConda = @(
+    "conda.exe",
+    "$HOME\Anaconda3\Scripts\conda.exe",
+    "C:\ProgramData\anaconda3\Scripts\conda.exe"
+)
+
+$ubicacionesValidas = @($ubicacionesConda | Where-Object{
+        Test-Path $_
+    })
+
+if($ubicacionesValidas.Length -gt 0){
+    $condaPath = $ubicacionesValidas[0]
+    Write-Host "Cargando entorno conda.."
+    (& $condaPath "shell.powershell" "hook") | `
+            Out-String | `
+            Where-Object{$_} | `
+            Invoke-Expression
+    Write-Host "Entorno conda CARGADO"
 }
 #endregion
 
