828f0d15bf2270b4871aabb3ac25df485e4b06a9
Herson <hersonx@gmail.com>
2025-07-10 23:56:25 -0600

    update

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,26 +1,2 @@
 
 
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-$ubicacionesConda = @(
-    "conda.exe",
-    "$HOME\Anaconda3\Scripts\conda.exe",
-    "C:\ProgramData\anaconda3\Scripts\conda.exe"
-)
-
-$ubicacionesValidas = @($ubicacionesConda | Where-Object{
-        Test-Path $_
-    })
-
-if($ubicacionesValidas.Length -gt 0){
-    $condaPath = $ubicacionesValidas[0]
-    Write-Host "Cargando entorno conda.."
-    (& $condaPath "shell.powershell" "hook") | `
-            Out-String | `
-            Where-Object{$_} | `
-            Invoke-Expression
-    Write-Host "Entorno conda CARGADO"
-}
-#endregion
-
-
