7c639be4685b46155c8a21528a719237dc76bbce
Herson-Laptop <hersonx@gmail.com>
2025-07-16 21:38:26 -0600

    Update del script "verificar_archivo.bat"

--- powershell/Scripts/verificar_archivo.bat
+++ powershell/Scripts/verificar_archivo.bat
@@ -3,6 +3,9 @@ setlocal
 
 REM Verificar que se haya pasado un argumento
 if "%~1"=="" (
+    echo.
+    echo DEBE ESCRIBIR LA DIRECCION DEL ARCHIVO O DIRECTORIO...
+    echo.
     echo Uso: %~nx0 ruta\al\archivo
     exit /b 1
 )
