cf4fbb296b29a3e3171c05e745df6075f6567245
Herson-Laptop <hersonx@gmail.com>
2025-07-20 14:46:05 -0600

    se cambia la forma como se obtiene el nombre del fichero mp3, antes de hacia con "Get-Item", ahora se usa el Json que genera el programa "ffmpeg.exe".

--- powershell/Scripts/InfoMedia.ps1
+++ powershell/Scripts/InfoMedia.ps1
@@ -44,8 +44,8 @@ $format = $infoJson.format
 $streams = $infoJson.streams
 
 Write-Host "`n========== INFORMACIÓN GENERAL ==========" -ForegroundColor Cyan
-#Write-Host "Nombre del archivo : $($format.filename)"
-Write-Host "Nombre del archivo : $((Get-Item $MediaFilePath).Name)"
+Write-Host "Nombre del archivo : $($format.filename)"
+#Write-Host "Nombre del archivo : $((Get-Item $MediaFilePath).Name)"
 Write-Host "Formato            : $($format.format_name)"
 Write-Host "Duración           : $(ConvertTo-CleanTime $format.duration)"
 Write-Host "Tamaño             : $(ConvertTo-FriendlySize $format.size)"
