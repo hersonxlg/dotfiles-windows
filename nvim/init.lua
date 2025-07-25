ccaf00dc14a58d32855484e05a688137cf780335
Herson-Laptop <hersonx@gmail.com>
2025-07-24 19:12:22 -0600

    update del fichero de configuración "lua/vim-options.lua", donde se le indica a la shell (pwsh.exe) que deben usar caracteres en codificación UTF-8.

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,8 +50,8 @@ RequireAll("autocmd")
 
 
 vim.opt.guicursor = {
-  'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
-  'c-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
+    'c-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
 }
