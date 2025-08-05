23ac4805a27c07829f4d7df2f2a146e49413d415
Herson <hersonx@gmail.com>
2025-08-05 15:54:54 -0600

    Actualización (update) del scritp "daudio.ps1", el cual, ahora recibe un segundo parametro opcional: "El nombre del audio que se descargara  con yt-dlp.exe"

--- powershell/Scripts/daudio.ps1
+++ powershell/Scripts/daudio.ps1
@@ -2,5 +2,12 @@
 # *******************************************************
 #            Comando "daudio"
 # *******************************************************
-param([string]$url)
-yt-dlp -f 'ba' -x --audio-format mp3 "$url"
+param(
+    [string]$url,
+    [string]$name = ""
+)
+if($name -eq ""){
+    yt-dlp -f 'ba' -x --audio-format mp3 "$url"
+} else{
+    yt-dlp -f 'ba' -x --audio-format mp3 -o "${name}.mp3" "$url"
+}
