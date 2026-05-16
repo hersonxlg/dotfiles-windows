2a7a2c9eff64cf61061cb526d71e22bab148fdb8
Herson Lozano <hersonx@gmail.com>
2026-05-16 08:31:53 -0600

    fix(treesitter): retrasar carga con eventos para eliminar parpadeo de error

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,20 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    -- Esto asegura que el plugin se descargue antes de intentar ejecutar nada
-    lazy = false, 
+    -- CAMBIO: Eliminamos 'lazy = false' y usamos eventos de lectura/creación de archivos.
+    -- Esto optimiza el arranque de Neovim y asegura que las rutas internas estén listas.
+    event = { "BufReadPost", "BufNewFile" }, 
     config = function()
         -- Usamos pcall (protected call) para evitar el pantallazo rojo
         local status, configs = pcall(require, "nvim-treesitter.configs")
         if not status then
-            -- Si falla, imprimimos un mensaje discreto y salimos de la función
+            -- Dejamos el aviso por si acaso, pero con los eventos de arriba ya nunca se ejecutará
             print("Treesitter aún no está listo. Ejecuta :Lazy sync")
             return
         end
 
         configs.setup({
-            -- Añade aquí tus lenguajes favoritos
+            -- Tus lenguajes favoritos
             ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "asm", "c" },
             highlight = { enable = true },
             indent = { enable = true },
