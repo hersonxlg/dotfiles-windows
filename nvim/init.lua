7035c542c12ff2d6f2911d985cb42f28e3ffee7c
Herson Lozano <hersonx@gmail.com>
2026-05-22 14:21:56 -0600

    refactor: migrar vim-options.lua a options.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,13 +17,10 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-
-
-
 ------------------------------------------------------------
 -- Configuraciones de NeoVim:
 ------------------------------------------------------------
-require("vim-options")
+require("options")
 
 ------------------------------------------------------------
 -- Configuraciones de NeoVim:
@@ -35,7 +32,6 @@ require("keymaps")
 ------------------------------------------------------------
 require("lazy").setup("plugins")
 
-
 ------------------------------------------------------------
 -- auto comando
 ------------------------------------------------------------
@@ -43,27 +39,23 @@ function RequireAll(relative_path)
     local path_base = vim.fn.expand("$LOCALAPPDATA\\nvim\\lua\\")
     vim.g.a = relative_path
     vim.g.b = path_base
-    local paths = vim.split(vim.fn.globpath(path_base .. relative_path, "*.lua"), '\n', { trimempty = true })
+    local paths = vim.split(vim.fn.globpath(path_base .. relative_path, "*.lua"), "\n", { trimempty = true })
     vim.g.p = ""
     for i, p in pairs(paths) do
         local path_for_require = p:gsub(path_base, ""):gsub(".lua", ""):gsub("\\", ".")
         require(path_for_require)
-        vim.g.p = vim.g.p .. path_for_require .. '\n'
+        vim.g.p = vim.g.p .. path_for_require .. "\n"
     end
 end
-
 
 ------------------------------------------------------------
 -- Comandos Automáticos
 ------------------------------------------------------------
 RequireAll("autocmd")
 
-
-
-
 vim.opt.guicursor = {
-    'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-    'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-    'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100',
-    'c-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+    "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+    "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+    "c-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
 }
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -24,7 +24,6 @@ vim.opt.backup = false
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
 vim.opt.undofile = true
 
-
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.isfname:append("@-@")
 -- vim.opt.guicursor = ""
@@ -39,15 +38,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Hinglight when yanking (copying) text",
     callback = function()
         vim.hl.on_yank()
-    end
+    end,
 })
 
 -----------------------------------------------------
 -- Simbolos sin representación gráfica:
 -----------------------------------------------------
 vim.o.list = true
-vim.o.listchars = 'tab:»·,lead:•,trail:•,eol:↲'
-
+vim.o.listchars = "tab:»·,lead:•,trail:•,eol:↲"
 
 -----------------------------------------------------
 -- Configurar el portapapeles (clipboard):
@@ -71,8 +69,6 @@ vim.opt.clipboard = "unnamedplus"
 --  end
 --  -- NOTA: En Linux, Neovim detectará automáticamente xclip, xsel o wl-copy gracias a unnamedplus.
 
-
-
 -----------------------------------------------------
 -- Configuración del Shell Interno (Multiplataforma)
 -----------------------------------------------------
@@ -82,7 +78,7 @@ if vim.fn.has("win32") == 1 then
     vim.o.shellquote = ""
     vim.o.shellxquote = ""
     vim.o.shellcmdflag =
-    "-NoLogo -NoProfile -Command [Console]::InputEncoding = [Console]::OutputEncoding = [Text.Encoding]::UTF8; $PSStyle.OutputRendering=[System.Management.Automation.OutputRendering]::PlainText;Remove-Alias -Name tee -Force -ErrorAction SilentlyContinue;"
+        "-NoLogo -NoProfile -Command [Console]::InputEncoding = [Console]::OutputEncoding = [Text.Encoding]::UTF8; $PSStyle.OutputRendering=[System.Management.Automation.OutputRendering]::PlainText;Remove-Alias -Name tee -Force -ErrorAction SilentlyContinue;"
     vim.o.shellpipe = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
     vim.o.shellredir = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 else
@@ -96,4 +92,24 @@ else
     end
 end
 
+-----------------------------------------------------
+-- Folding (plegado) para Neovm 0.12:
+-----------------------------------------------------
 
+-- 1. Decirle a Neovim que use expresiones para el plegado
+vim.opt.foldmethod = "expr"
+
+-- 2. Usar la función nativa de Tree-sitter para calcular los pliegues
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- 3. Evitar que todo el código se abra completamente colapsado al iniciar un archivo
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+
+-- 4. Truco visual moderno: mantiene el resaltado de sintaxis (colores) en la línea plegada
+vim.opt.foldtext = ""
+
+-- 5. Opcional: Muestra una columna a la izquierda indicando dónde hay pliegues
+-- "0" para ocultarla, "1" para verla de forma discreta
+vim.opt.foldcolumn = "1"
